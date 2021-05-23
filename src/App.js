a67af7a027765b349e6fd2fe7075bcae6c3904de
Gardner Bickford <gardner@bickford.nz>
2021-05-23 22:45:52 +1200

    Add fetch() for example data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function getPosts() {
+      const result = await fetch('https://jsonplaceholder.typicode.com/posts/')
+      .then(data => data.json());
+      setPosts(result);
+    }
+    getPosts()
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>
+          <b>{post.title}</b>
+          <p>{post.body}</p>
+        </li>
+      ))}
+    </ul>
   );
 }
 
